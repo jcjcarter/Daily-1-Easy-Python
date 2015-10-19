0c6e3bcdfae27db5bc6368007c0922436264df17
jcjcarter <allstatetexan@hotmail.com>
2015-10-19 13:46:29 -0500

    Initial.

---
+++ Daily 1 Easy Python/Daily_1_Easy_Python.py
@@ -0,0 +1,10 @@
+# User's name.
+name = input("What is your name? ")
+
+# User's age.
+age = input("How old are you? ")
+
+# User's username.
+username = input("What is your username? ")
+
+print('Your name is  {0}, you are {1} years old, and your username is {2}.'.format(name, age, username))
